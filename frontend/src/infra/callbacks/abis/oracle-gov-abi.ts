d9148a9a1edbd0b26e06f65cddf57945c9a4e82a
renancorreadev <producer@skynance.net>
2022-12-15 04:51:22 -0300

    feat: mocking

---
+++ frontend/src/infra/callbacks/abis/oracle-gov-abi.ts
@@ -0,0 +1,199 @@
+export const abi = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "_idImob",
+        "type": "uint256"
+      }
+    ],
+    "name": "ConsultImobReg",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "_registryRural",
+        "type": "uint256"
+      }
+    ],
+    "name": "ConsultMapRural",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "_map",
+        "type": "string"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "_registryRural",
+        "type": "uint256"
+      }
+    ],
+    "name": "confrontMapEvent",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_idImob",
+        "type": "uint256"
+      }
+    ],
+    "name": "ConsultRegular",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "regular",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_map",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_registryRural",
+        "type": "uint256"
+      }
+    ],
+    "name": "confrontMap",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_registryRural",
+        "type": "uint256"
+      }
+    ],
+    "name": "consultMap",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "regular",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numberImobRegular",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numberMapRegularRural",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_addr",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "_allow",
+        "type": "bool"
+      }
+    ],
+    "name": "setAllow",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_maprural",
+        "type": "string"
+      }
+    ],
+    "name": "setMapRural",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_idImob",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "_regular",
+        "type": "bool"
+      },
+      {
+        "internalType": "address",
+        "name": "_imobcontract",
+        "type": "address"
+      }
+    ],
+    "name": "setRegular",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+] as const
--- frontend/src/main/factories/pages/Query/index.tsx
+++ frontend/src/main/factories/pages/Query/index.tsx
@@ -1,12 +1,141 @@
-import { FunctionComponent ,useState} from "react";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { FunctionComponent ,useEffect,useState} from "react";
 import styles from "./Consulta.module.css";
+
+import {readContract} from '@wagmi/core'
+import {ConsultContractConfig} from '../../../../utils/ContractConfigs'
+import {abi} from '../../../../infra/callbacks/abis/oracle-gov-abi'
+import { BigNumber } from 'ethers';
+
+import Modal from 'react-modal';
+import { CircleLoader } from "react-spinners";
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
 
 export const Query: FunctionComponent = () => {
 
   const [matricula, setMatricula] = useState("");
-  const [comarcaImb, setComarcaImb] = useState("");
-  const [comarca, setComarca] = useState("");
-  const [mapAddress, setMapAddress] = useState("");
+  const [numeroMatricula, setNumeroMatricula] = useState("");
+  const [uriMap, setURIMap] = useState("");
+  const [resultConsultRegular, setResultConsultRegular] = useState(false)
+  const [resultConfrontMap, setResultConfrontMap] = useState<any>()
+
+  const [openResultA, setOpenResultA] = useState(false)
+  const [openResultB, setOpenResultB] = useState(false)
+
+  const [circleLoading, setCircleLoading] = useState(true);
+
+
+  const address = '0xB5F72BB1BCbB9f9564cfc46cD74899227366428F'
+
+
+
+  const getRegular = async() : Promise<boolean | undefined> => {
+      try {
+        const txResult = await readContract({
+          address,
+          abi,
+          functionName: 'ConsultRegular' ,
+          args: [
+            BigNumber.from(matricula),
+          ],
+        });
+
+        return txResult
+      } catch (e) {
+        console.log(e)
+      }
+
+      return
+  }
+
+  const getconfrontMap = async() => {
+    try {
+      const txResult = await readContract({
+        address,
+        abi,
+         //@ts-ignore
+        functionName: 'confrontMap' ,
+        args: [
+         uriMap,
+         BigNumber.from(numeroMatricula)
+        ]
+      });
+
+      return txResult
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  // const getConsultMap = async() : Promise<boolean | undefined> => {
+  //   try {
+  //     const txResult = await readContract({
+  //       address,
+  //       abi,
+  //       functionName: 'consultMap' ,
+  //       args: [
+  //         BigNumber.from(mapAddress),
+  //       ]
+  //     });
+
+  //     return txResult
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+
+  //   return
+  // }
+
+  async function handleGetRegular() {
+    const result = await getRegular() as boolean
+    setResultConsultRegular(result)
+
+    setOpenResultA(true)
+  }
+
+  async function handleGetConfrontMap() {
+    const resulttx = await getconfrontMap().then(result => {
+      return result
+    })
+
+    setResultConfrontMap(resulttx)
+
+    setOpenResultB(true)
+
+  }
+
+  console.log(resultConsultRegular)
+  console.log(resultConfrontMap)
+
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
+
+  function closeModal() {
+    setmodalIsOpen(false);
+  }
+
+
+  useEffect(() => {
+    if(openResultA || !openResultA){
+      setmodalIsOpen(true)
+    }
+  }, [openResultA ])
+
+  useEffect(() => {
+    if(openResultB || !openResultB){
+      setmodalIsOpen(true)
+    }
+  }, [openResultB ])
 
   return (
     <div  className={styles.Container}>
@@ -28,34 +157,24 @@ export const Query: FunctionComponent = () => {
               required className={styles.input}
               placeholder="Numero da Matricula: "
             />
-            <input
-              onChange={(e) => setComarca(e.target.value)}
-              type="text"
-              id="name"
-              name="numberOficio"
-              required className={styles.input}
-              placeholder="Comarca do imóvel: "
-            />
 
             <button className={styles.group3}
-              onClick={() => {
-                console.log
-              }}>
+              onClick={handleGetRegular}>
               <span>Consultar</span>
             </button>
           </div>
           <div className={styles.Quad2}>
             <input
-              onChange={(e) => setMapAddress(e.target.value)}
+              onChange={(e) => setURIMap(e.target.value)}
               type="text"
               id="name"
               name="numberOficio"
               required className={styles.input}
-              placeholder="Numero da Matricula: "
+              placeholder="Insira o link referente ao mapa da região"
             />
 
             <input
-              onChange={(e) => setComarcaImb(e.target.value)}
+              onChange={(e) => setNumeroMatricula(e.target.value)}
               type="text"
               id="name"
               name="numberOficio"
@@ -64,12 +183,13 @@ export const Query: FunctionComponent = () => {
             />
 
             <button className={styles.group3}
-              onClick={() => {
-                console.log
-              }}>
+              onClick={handleGetConfrontMap}>
               <span>Consultar</span>
             </button>
           </div>
+
+
+
       </div>
 
       <div className={styles.footerParagraph}>
@@ -78,6 +198,68 @@ export const Query: FunctionComponent = () => {
           feita por Oráculo que usam dados fornecidos por entes autorizados.
         </p>
       </div>
+
+      {openResultA && (
+          <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              style={customStyles}
+              contentLabel="Modal Minting"
+              >
+              <button
+              onClick={() => setmodalIsOpen(!modalIsOpen)}
+              className={styles.closeModal}
+            >X</button>
+              <div className={styles.status}>
+                  <h2 className={styles.titleModal}>Resultado</h2>
+                  <p className={styles.paragraphModal}>{
+                      resultConsultRegular ? 'O imóvel está regularizado' : 'O imóvel não está regularizado'
+                  }</p>
+
+              <div className={styles.imageLoadingContainer}>
+                <CircleLoader
+                  color="#5400d3"
+                  loading={circleLoading}
+                  size={450}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+              </div>
+              </div>
+          </Modal>
+          )}
+
+      {openResultB && (
+          <Modal
+          isOpen={modalIsOpen}
+
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Modal Minting"
+          >
+          <button
+          onClick={() => setmodalIsOpen(!modalIsOpen)}
+          className={styles.closeModal}
+        >X</button>
+          <div className={styles.status}>
+              <h2 className={styles.titleModal}>Resultado</h2>
+              <p className={styles.paragraphModal}>{
+                    resultConfrontMap ?  'O imóvel está regularizado' : 'O imóvel não está regularizado'
+              }</p>
+
+              <div className={styles.imageLoadingContainer}>
+                <CircleLoader
+                  color="#5400d3"
+                  loading={circleLoading}
+                  size={450}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+            </div>
+          </div>
+      </Modal>
+      )}
+
   </div>
   );
 };
