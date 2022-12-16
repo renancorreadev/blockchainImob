5f84c184f87963cef0b4e3e1010c80e5f467a93f
renancorreadev <producer@skynance.net>
2022-12-16 01:35:45 -0300

    test: ensure test blockimob-reads test callbacks

---
+++ frontend/src/data/useCases/blockimob-reads-calls.spec.tsx
@@ -0,0 +1,58 @@
+import { BlockImobReadContract } from "./blockimob-reads-calls";
+import { describe, test, vi, beforeEach } from "vitest";
+
+import { Connect } from "@test/utils/connect";
+import { render } from "@test/index";
+
+type SutTypes = {
+  sut: BlockImobReadContract;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new BlockImobReadContract();
+  return {
+    sut,
+  };
+};
+
+describe("BlockImobReadContract", () => {
+  render(
+    <>
+      <Connect />
+    </>
+  );
+
+  beforeEach(() => {
+    // Reset the mock
+    vi.clearAllMocks();
+  });
+
+  test("Shold function ContractName is called ", async () => {
+    const { sut } = makeSut();
+    const getContractNameSpy = vi.spyOn(sut, "getContractNameRequest");
+
+    // Call the function for the check
+    sut.getContractNameRequest();
+    expect(getContractNameSpy).toHaveBeenCalled();
+  });
+
+  test("Should ensure to called times in contract", () => {
+    const { sut } = makeSut();
+    const getContractNameSpy = vi.spyOn(sut, "getContractNameRequest");
+
+    sut.getContractNameRequest();
+    sut.getContractNameRequest();
+    //toHaveBeenCalledTimes to ensure that a mock function got called exact number of times.
+    expect(getContractNameSpy).toHaveBeenCalledTimes(2);
+  });
+
+  test("Shold the result is correct!", () => {
+    const { sut } = makeSut();
+
+    const resultMock = vi
+      .fn()
+      .mockImplementation(async () => sut.getContractNameRequest());
+
+    resultMock.mockReturnValue("BlockImob");
+  });
+});
