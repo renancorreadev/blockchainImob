367390cdcc297d182e365de179676b9eea4f32c1
renancorreadev <producer@skynance.net>
2022-12-20 02:35:30 -0300

    test: ensure add consult oracle reads contracts

---
+++ frontend/src/data/useCases/consult-imob.reads.spec.ts
@@ -0,0 +1,59 @@
+import { describe, expect, Mock, vi } from "vitest";
+import { ConsultImobRead } from "./consult-imob-reads";
+import { BigNumber } from "ethers";
+import {
+  getConsultRegular,
+  getConsultMap,
+  MockParams,
+} from "../tests/mock-consult.reads";
+
+type MakeSutTypes = {
+  _mock: ConsultImobRead;
+  fakeID: BigNumber;
+  getConsultRegular: Mock<[mockParams: MockParams], Promise<boolean>>;
+  getConsultMap: Mock<[mockParams: MockParams], Promise<boolean>>;
+};
+
+const makeSut = (): MakeSutTypes => {
+  const _mock = new ConsultImobRead();
+  const fakeID = BigNumber.from(1);
+
+  return {
+    _mock,
+    fakeID,
+    getConsultRegular,
+    getConsultMap,
+  };
+};
+
+describe("Should testing Read Contract Returns Consult Oracle", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test("should return the consult Map per ID is True or False", async () => {
+    const { getConsultMap, _mock, fakeID } = makeSut();
+
+    getConsultMap.mockResolvedValue(true);
+
+    const fetchResponse = await getConsultMap({
+      _mockGetConsultContract: _mock,
+      args: fakeID,
+    });
+
+    expect(fetchResponse).toBe(true);
+  });
+
+  test("should return the consult Regular is True or False", async () => {
+    const { getConsultRegular, _mock, fakeID } = makeSut();
+
+    getConsultRegular.mockResolvedValue(true);
+
+    const fetchResponse = await getConsultRegular({
+      _mockGetConsultContract: _mock,
+      args: fakeID,
+    });
+
+    expect(fetchResponse).toBe(true);
+  });
+});
