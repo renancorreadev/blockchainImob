e5cd5ed4243681d6ee6b582b5b68f252dde4584b
renancorreadev <producer@skynance.net>
2022-12-20 01:04:53 -0300

    test: implement approve transaction in dataLayer

--- frontend/src/data/useCases/block-imob-writes.spec.ts
+++ frontend/src/data/useCases/block-imob-writes.spec.ts
@@ -1,11 +1,14 @@
-import { BigNumber } from "ethers";
 import { vi } from "vitest";
-import { BlockImobWrites, MintDataReturnType } from "./block-imob-writes";
+import { BigNumber } from "ethers";
+import { BlockImobWrites } from "./block-imob-writes";
+
+import { TransactionError } from "@domain/errors/error-transaction";
+import { ApproveArgsInterface, TransactionReceipType } from "@data/protocols";
 
 const makeSut = () => {
   /** Constants values  */
   const _mock = new BlockImobWrites();
-  const returnPromised: MintDataReturnType = {
+  const returnPromised: TransactionReceipType = {
     blockNumber: 1,
     gasUsed: BigNumber.from(30000000),
     transactionHash: "0x123",
@@ -23,10 +26,15 @@ const makeSut = () => {
     });
   });
 
+  const mockApproveTest = vi.fn((approveArgs: ApproveArgsInterface) => {
+    return _mock.approve(approveArgs);
+  });
+
   return {
     _mock,
     returnPromised,
     mockMintTest,
+    mockApproveTest,
   };
 };
 
@@ -42,5 +50,33 @@ describe("Shold be callbacks contracts write transactions", () => {
 
     const result = await mockMintTest();
     expect(result).toStrictEqual(returnPromised);
+  });
+
+  test("Should be call transaction to approve a NFT transfer", async () => {
+    const { returnPromised, mockApproveTest } = makeSut();
+    const callMocked = mockApproveTest.mockResolvedValue(returnPromised);
+    const _address =
+      "0x431143aa81Aa54fB4157edcb8879d23c1eff9c77" as `0x${string}`;
+
+    const fakeArgs = {
+      spender: _address,
+      id: BigNumber.from(1),
+    };
+
+    const result = await mockApproveTest(fakeArgs);
+
+    expect(callMocked).toBeCalled();
+    expect(callMocked).toBeCalledWith(fakeArgs);
+    expect(result).toStrictEqual(returnPromised);
+  });
+
+  test("Should be Invalid call transaction to approve and return Error", async () => {
+    const { mockApproveTest } = makeSut();
+
+    const result = mockApproveTest.mockRejectedValueOnce(
+      new TransactionError()
+    );
+
+    await expect(result).rejects.toThrowError(new TransactionError());
   });
 });
