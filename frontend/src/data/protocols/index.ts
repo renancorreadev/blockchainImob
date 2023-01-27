b22f196bb6b180cca17dcfcd818010c2bcda7b55
renancorreadev <producer@skynance.net>
2023-01-27 18:25:19 -0300

    fix: adjusts to deploy

--- frontend/src/data/protocols/index.ts
+++ frontend/src/data/protocols/index.ts
@@ -1,3 +1,3 @@
 export * from "./block-imob-reads-protocol";
-export * from "./block-Imob-writes-protocol";
+export * from "./block-imob-writes-protocol";
 export * from "./consult-gov-reads-protocol";
